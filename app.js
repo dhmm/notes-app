620465fd9f4360de405dcf885f93219259cdda91
dhmm <dhmm@outlook.com>
2022-07-04 16:19:48 +0300

    wip

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const fs = require('fs');
-fs.appendFileSync('notes.txt' , 'Second line\n');
+// // const fs = require('fs');
+// // fs.appendFileSync('notes.txt' , 'Second line\n');
+
+// const add = require('./utils.js')
+
+// console.log(add(9,8));
+
+
+const getNotes = require('./notes.js');
+console.log(getNotes());
---
+++ utils.js
@@ -0,0 +1,7 @@
+console.log('utils.js');
+
+const add = function (a, b) {
+  return a + b;
+}
+
+module.exports = add;
